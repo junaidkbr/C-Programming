ee7b801fa2a8b6c56fd5db346cbf08e266d5b860
uzair ahmad <uzairdev8@gmail.com>
2022-10-22 11:46:22 +0500

    h1 added

--- Basic Problem's/Absolute-value.c
+++ Basic Problem's/Absolute-value.c
@@ -1,19 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+This is Testing
 
-int main(){
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// int main(){
     
-   int x,y ;
+//    int x,y ;
     
-	printf("Enter 1st Number : ");
-   	scanf("%d", &x);
+// 	printf("Enter 1st Number : ");
+//    	scanf("%d", &x);
     
-   	printf("Enter 2nd Number : ");
-   	scanf("%d", &y);
+//    	printf("Enter 2nd Number : ");
+//    	scanf("%d", &y);
     
-    printf("Absolute value of %d is %d \n", x , labs(x));
-    printf("Absolute value of %d is %d \n", y , labs(y));
+//     printf("Absolute value of %d is %d \n", x , labs(x));
+//     printf("Absolute value of %d is %d \n", y , labs(y));
     
     
-    return 0;
-}
+//     return 0;
+// }
